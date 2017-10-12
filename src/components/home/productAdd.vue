2a46103c0c5d41ec5b6d15ec1e5075bdcde68eaa
luogf <zoroshow@outlook.com>
2017-10-12 15:12:01 +0800

    修改产品添加页面

--- src/components/home/productAdd.vue
+++ src/components/home/productAdd.vue
@@ -1,37 +1,81 @@
 <style scoped>
-  .formAdd {
-    width: 500px;
+  .add {
+    width: 50%;
     position: relative;
-    left: 50px;
-    height: 100%;
+    left: 20%;
   }
 </style>
 <template>
-  <Form ref="productInfo" :model="productInfo" inline>
-    <FormItem prop="name">
-      <Input type="text" v-model="productInfo.name" placeholder="产品名称"></Input>
-    </FormItem>
-    <FormItem prop="type">
-      <Input type="text" v-model="productInfo.type" placeholder="产品型号"></Input>
-    </FormItem>
-    <FormItem prop="factory">
-      <Input type="text" v-model="productInfo.factory" placeholder="生产厂家"></Input>
-    </FormItem>
-    <FormItem prop="order">
-      <Input type="text" v-model="productInfo.order" placeholder="产品指令"></Input>
-    </FormItem>
-  </Form>
+  <div class="add">
+    <Form :model="formItem" :label-width="80">
+      <FormItem label="输入框">
+        <Input v-model="formItem.input" placeholder="请输入"></Input>
+      </FormItem>
+      <FormItem label="选择器">
+        <Select v-model="formItem.select" placeholder="请选择">
+          <Option value="beijing">北京市</Option>
+          <Option value="shanghai">上海市</Option>
+          <Option value="shenzhen">深圳市</Option>
+        </Select>
+      </FormItem>
+      <FormItem label="日期控件">
+        <Row>
+          <i-col span="11">
+            <DatePicker type="date" placeholder="选择日期" v-model="formItem.date"></DatePicker>
+          </i-col>
+          <i-col span="2" style="text-align: center">-</i-col>
+          <i-col span="11">
+            <TimePicker type="time" placeholder="选择时间" v-model="formItem.time"></TimePicker>
+          </i-col>
+        </Row>
+      </FormItem>
+      <FormItem label="单选框">
+        <RadioGroup v-model="formItem.radio">
+          <Radio label="male">男</Radio>
+          <Radio label="female">女</Radio>
+        </RadioGroup>
+      </FormItem>
+      <FormItem label="多选框">
+        <CheckboxGroup v-model="formItem.checkbox">
+          <Checkbox label="吃饭"></Checkbox>
+          <Checkbox label="睡觉"></Checkbox>
+          <Checkbox label="跑步"></Checkbox>
+          <Checkbox label="看电影"></Checkbox>
+        </CheckboxGroup>
+      </FormItem>
+      <FormItem label="开关">
+        <i-switch v-model="formItem.switch" size="large">
+          <span slot="open">开启</span>
+          <span slot="close">关闭</span>
+        </i-switch>
+      </FormItem>
+      <FormItem label="滑块">
+        <Slider v-model="formItem.slider" range></Slider>
+      </FormItem>
+      <FormItem label="文本域">
+        <Input v-model="formItem.textarea" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="请输入..."></Input>
+      </FormItem>
+      <FormItem>
+        <Button type="primary">提交</Button>
+        <Button type="ghost" style="margin-left: 8px">取消</Button>
+      </FormItem>
+    </Form>
+  </div>
 </template>
-
 <script>
   export default {
     data () {
       return {
-        productInfo: {
-          name: '',
-          type: '',
-          order: '',
-          factory: '',
+        formItem: {
+          input: '',
+          select: '',
+          radio: 'male',
+          checkbox: [],
+          switch: true,
+          date: '',
+          time: '',
+          slider: [20, 50],
+          textarea: ''
         }
       }
     }
