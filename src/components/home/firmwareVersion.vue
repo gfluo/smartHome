2b168b9dc47c012344ab4276b270e03f5b30bc20
luogf <zoroshow@outlook.com>
2017-10-16 14:16:34 +0800

    修改关键管理页面

--- src/components/home/firmwareVersion.vue
+++ src/components/home/firmwareVersion.vue
@@ -13,7 +13,7 @@
 </style>
 <template>
   <div class="full">
-      <div>  
+      <div>
         <Select v-model="defaultValue" style="width:200px">
             <Option v-for="item in productList" :value="item.value" :key="item.value">{{ item.label }}</Option>
         </Select>
@@ -23,6 +23,20 @@
             <Button type='primary' icon="plus-round">新增</Button>
           </div>
           <Table stripe :columns="header" :data="rows"></Table>
+      </div>
+      <div style="background:#eee;position: relative;top: 10px">
+        <Card :bordered="false">
+          <p slot="title">固件ID: 0000001</p>
+          <p><span>固件版本：</span>1.0</p>
+          <p><span>固件大小：</span>3.0 Mb</p>
+          <p><span>固件适配产品：</span>利思比 P8C中控</p>
+          <p><span>固件上传时间：</span>2017-10-15</p>
+          <p><span>是否测试通过：</span>是</p>
+          <p><span>用户安装数：</span>2000</p>
+          <p><span>固件类型：</span>????</p>
+          <p><span>识别码：</span>????</p>
+          <p><span>MD5校验码：</span>????</p>
+        </Card>
       </div>
   </div>
 </template>
@@ -36,12 +50,15 @@
                     label: 'P8C中控',
                 }],
                 header: [{
+                    title: '版本ID',
+                    key: 'id',
+                },{
                     title: '版本号',
                     key: 'value',
                 }, {
                     title: '设备安装数量',
                     key: 'count',
-                }, { 
+                }, {
                     title: '占比',
                     key: 'offTotal',
                 },{
@@ -82,14 +99,16 @@
             }
           }],
                 rows: [{
+                    id: '00000001',
                     value: '1.0',
                     count: '2000',
                     offTotal: '20%',
                 }, {
+                  id: '00000002',
                     value: '2.0',
                     count: '8000',
                     offTotal: '80%',
-                }], 
+                }],
             }
         }
     }
